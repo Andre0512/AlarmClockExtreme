053329c31764e9cb7e4a3ddf7d5b0b1877fa6fcd
Andre <andre.basche@gmail.com>
2018-12-08 14:04:24 +0100

    Improve logging

--- alarm.py
+++ alarm.py
@@ -5,6 +5,7 @@ import os
 import sys
 import threading
 import time
+import timeit
 
 # Needed for running on LibreELEC
 sys.path.append("/storage/.kodi/addons/virtual.rpi-tools/lib")
@@ -19,8 +20,6 @@ import requests
 
 from secrets import IP, API_KEY
 
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format='%(levelname)s - %(message)s')
-
 BUZZER_CTRL_PIN = 2
 BUTTON_CTRL_PIN = 18
 WATER_BOILER_SWITCH = 6
@@ -34,6 +33,14 @@ TRY_DURATION = 180
 SNOOZE = 180
 SHIT_PAUSE = 300
 WAKE_UP_TRIES = 30
+DEBUG = False
+
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format='%(levelname)s - %(message)s')
+else:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+                        filename="{}/{}".format(os.path.dirname(os.path.realpath(__file__)), 'alarm.log'))
+logger = logging.getLogger(__name__)
 
 
 def setup_gpio():
@@ -48,7 +55,7 @@ def close_gpio():
 
 def beep(duration, pause):
     try:
-        logging.debug("Beeeeeep {} senconds".format(duration))
+        logger.debug("Beeeeeep {} senconds".format(duration))
         GPIO.output(BUZZER_CTRL_PIN, GPIO.HIGH)
         time.sleep(duration)
         GPIO.output(BUZZER_CTRL_PIN, GPIO.LOW)
@@ -60,16 +67,20 @@ def beep(duration, pause):
 def check_button():
     for check in range(TRY_DURATION * 20):
         if not check % 100 and get_humidity() > SHOWER_TRIGGER * 100:
+            logger.info("Stopped beeping after {} seconds because humidity is at {}%".format(str(float(check / 20)), str(
+                float(get_humidity()) / 100)))
             return
         if not GPIO.input(BUTTON_CTRL_PIN):
+            logger.info("Stopped beeping because button was pressed after {} seconds".format(str(float(check / 20))))
             return
         time.sleep(TRY_DURATION / 20)
+    logger.info("Stopped beeping because no reaction after {} seconds ".format(str(float(check / 20))))
 
 
 def piep_thread(stop):
     while True:
         if not stop.is_set():
-            logging.debug("Start playing Melody")
+            logger.debug("Start playing Melody")
             for tone in BEEP_MELODY:
                 beep(float(tone) / 4, 0.1)
         time.sleep(2)
@@ -78,7 +89,7 @@ def piep_thread(stop):
 def get_humidity():
     response = requests.get("http://{}/api/{}/sensors/{}".format(IP, API_KEY, HUMIDITY_SENSOR)).json()
     result = int(response['state']['humidity'])
-    logging.debug("Humidity: {}%".format(str(float(result) / 100)))
+    logger.debug("Humidity: {}%".format(str(float(result) / 100)))
     return result
 
 
@@ -90,10 +101,10 @@ def start_stop_alarm(value):
 
 
 def main():
+    start = timeit.default_timer()
     try:
         start_stop_alarm(True)
         time.sleep(COFFEE_LEAD_TIME)
-        setup_gpio()
         t_stop = threading.Event()
         t = threading.Thread(target=piep_thread, args=(t_stop,))
         t.start()
@@ -105,11 +116,16 @@ def main():
             close_gpio()
             t_stop.set()
             time.sleep(SNOOZE if stop_timer < WAKE_UP_TRIES else SNOOZE + SHIT_PAUSE)
-            logging.debug("Try round " + str(WAKE_UP_TRIES - stop_timer))
+            logger.debug("Try round " + str(WAKE_UP_TRIES - stop_timer))
             stop_timer = stop_timer - 1
+        if not stop_timer:
+            logger.info("Stopped because humidity is at {}%".format(get_humidity()))
+        else:
+            logger.info("Giving up to wake up after {} snoozes".format(WAKE_UP_TRIES))
     finally:
         close_gpio()
         start_stop_alarm(False)
+    logger.info("Exit after {} seconds".format(start - timeit.default_timer()))
 
 
 if __name__ == "__main__":
