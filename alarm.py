ddc51e6a52cf37e67c7663d9de65020c401fcebf
Andre <andre.basche@gmail.com>
2018-12-08 13:13:35 +0100

    Sourced out constants

--- alarm.py
+++ alarm.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python
 
+import logging
 import sys
+import threading
+import time
 
 # Needed for running on LibreELEC
 sys.path.append("/storage/.kodi/addons/virtual.rpi-tools/lib")
-
-import RPi.GPIO as GPIO
-import time
-import threading
-import sys
-
 sys.path.append('/storage/.kodi/addons/script.module.requests/lib/')
 sys.path.append('/storage/.kodi/addons/script.module.urllib3/lib/')
 sys.path.append('/storage/.kodi/addons/script.module.chardet/lib/')
 sys.path.append('/storage/.kodi/addons/script.module.certifi/lib/')
 sys.path.append('/storage/.kodi/addons/script.module.idna/lib/')
+
+import RPi.GPIO as GPIO
 import requests
-import logging
 
 from secrets import IP, API_KEY
 
@@ -24,6 +22,15 @@ logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format='%(levelname)
 
 BUZZER_CTRL_PIN = 2
 BUTTON_CTRL_PIN = 18
+WATER_BOILER_SWITCH = 6
+HUMIDITY_SENSOR = 54
+
+SHOWER_TRIGGER = 85  # Humidity in percent
+COFFEE_LEAD_TIME = 180
+TRY_DURATION = 180
+SNOOZE = 180
+SHIT_PAUSE = 300
+WAKE_UP_TRIES = 30
 
 
 def setup():
@@ -38,7 +45,7 @@ def close():
 
 def piep(duration, pause):
     try:
-        logging.debug("piiiieeeeppp")
+        logging.debug("Beeeeeep {} senconds".format(duration))
         GPIO.output(BUZZER_CTRL_PIN, GPIO.HIGH)
         time.sleep(duration)
         GPIO.output(BUZZER_CTRL_PIN, GPIO.LOW)
@@ -48,19 +55,20 @@ def piep(duration, pause):
 
 
 def check_button():
-    for x in range(3600):
+    for x in range(TRY_DURATION * 20):
         if not x % 100:
-            humidity = int(requests.get("http://{}/api/{}/sensors/54".format(IP, API_KEY)).json()['state']['humidity'])
+            humidity = int(
+                requests.get("http://{}/api/{}/sensors/{}".format(IP, API_KEY, HUMIDITY_SENSOR)).json()['state'][
+                    'humidity'])
             logging.debug("Humidity: " + str(float(humidity) / 100) + "%")
-            if humidity > 8500:
+            if humidity > SHOWER_TRIGGER * 100:
                 return
         if not GPIO.input(BUTTON_CTRL_PIN):
             return
-        time.sleep(0.05)
+        time.sleep(TRY_DURATION / 20)
 
 
 def ducks():
-    # Alle meine Entchen
     beat = [1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 4]
     for x in beat:
         piep(float(x) / 4, 0.1)
@@ -76,27 +84,26 @@ def piep_thread(stop):
 
 def main():
     try:
-        requests.put("http://{}/api/{}/lights/6/state".format(IP, API_KEY), data='{"on": true}')
-        time.sleep(180)
+        requests.put("http://{}/api/{}/lights/{}/state".format(IP, API_KEY, WATER_BOILER_SWITCH), data='{"on": true}')
+        time.sleep(COFFEE_LEAD_TIME)
         setup()
         t_stop = threading.Event()
         t = threading.Thread(target=piep_thread, args=(t_stop,))
         t.start()
-        stop_timer = 30
-        while int(requests.get("http://{}/api/{}/sensors/54".format(IP, API_KEY)).json()['state'][
-                      'humidity']) < 8500 and stop_timer:
+        stop_timer = WAKE_UP_TRIES
+        while int(requests.get("http://{}/api/{}/sensors/{}".format(IP, API_KEY, HUMIDITY_SENSOR)).json()['state'][
+                      'humidity']) < SHOWER_TRIGGER * 100 and stop_timer:
             setup()
             t_stop.clear()
             check_button()
             close()
             t_stop.set()
-            time.sleep(180)
-            time.sleep(10)
+            time.sleep(SNOOZE if stop_timer < WAKE_UP_TRIES else SNOOZE + SHIT_PAUSE)
+            logging.debug("Round: " + str(WAKE_UP_TRIES - stop_timer))
             stop_timer = stop_timer - 1
-            logging.debug("Round: " + str(31 - stop_timer))
     finally:
         close()
-        requests.put("http://{}/api/{}/lights/6/state".format(IP, API_KEY), data='{"on": false}')
+        requests.put("http://{}/api/{}/lights/{}/state".format(IP, API_KEY, WATER_BOILER_SWITCH), data='{"on": false}')
 
 
 if __name__ == "__main__":
